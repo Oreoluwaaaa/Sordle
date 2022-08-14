acb3987d06c67b67693fe0223de616fc588b42ea
Oreoluwaaaa <robotics717@gmail.com>
2022-08-14 00:55:01 -0400

    check text against answer

--- src.js
+++ src.js
@@ -7,6 +7,36 @@ function insert(row, input){
     }
 }
 
+async function inDictionary(word){
+    let inDict = false;
+    try {
+        await $.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`, function(data) {
+            inDict = true;
+        });
+    }
+    catch(error){
+        inDict = false;
+    }
+    return inDict;
+}
+
+function checkInput(input, answer, row){
+    correctInput = 0;
+    for(let j = 0; j < 7; j++){
+        if(input[j] === answer[j]){
+            $(`div.row#${row} > div#${j}.cell`).css("background", "green");
+            correctInput++;
+        }
+        else if(answer.includes(input[j])){
+            $(`div.row#${row} > div#${j}.cell`).css("background", "yellow");
+        }
+        else{
+            $(`div.row#${row} > div#${j}.cell`).css("background", "black");
+        }
+    }
+    return correctInput === 7 ? true: false;
+}
+
 $( document ).ready(function() {
     for(i = 0; i < 7; i++){
         $("#table").append(`<div class='row' id=${i}></div>`);
@@ -17,29 +47,29 @@ $( document ).ready(function() {
             $(element).append(`<div class='cell' id=${i}></div>`);
         }    
     });
-
+    let answer = "prayers";
     let input = ""; 
     let row = 0;
     $(document).keyup(function(event) {
         keypressed = event.originalEvent.key;
-        
-        if (!/[^a-z$]/.test(keypressed)){
+        console.log(keypressed)
+        if (!/[^a-z$]/.test(keypressed) && row < 7){
             input += (input.length < 7) ? event.originalEvent.key : "";
             console.log(input);
             insert(row, input);
         }
-
         else if (keypressed == "Backspace"){
             input = input.substring(0, input.length-1);
             insert(row, input);
         }
-
-        else if (keypressed == "Enter"){
-            console.log("pressed enter");
-        }
-
-        else{
-            console.log("Wrong");
+        else if ((keypressed == "Enter") && (input.length === 7)){
+            inDictionary(input).then(result => {
+                if(result){
+                    checkInput(input, answer, row);
+                    row++;
+                    input =  "";    
+                }
+            })
         }
     });
 });
